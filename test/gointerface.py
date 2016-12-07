e16122f098fcd6c9ba3ac46003850ac923c775d3
Yihe Huang <yihehuang@g.harvard.edu>
2016-12-07 15:02:13 -0500

    gointerface.py now talks over network

--- test/gointerface.py
+++ test/gointerface.py
@@ -3,28 +3,48 @@ import rlp
 from gen_freq_opcode import opcode_table
 from gen_freq_opcode import inv_opcode_table
 from bitarray import bitarray
-from optparse import OptionParser
 import binascii
-import sys
+import sys,socket
 
-parser = OptionParser()
-parser.add_option("-b", "--bytecode", dest="bytecode",
-                          help="EVM bytecode as hex string")
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
 
-(options, args) = parser.parse_args()
-hbytecode = options.bytecode
-#hbytecode = sys.argv[1]
+huffman_file = 'huffmanopcodes.txt'
 
+def start_server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen(1)
+    print 'Listening on %s:%d' % (TCP_IP, TCP_PORT)
+    return s
+
+def accept_and_handle(s, hcode_table):
+    conn, addr = s.accept()
+    print 'Accepted connection from ', addr
+    f = conn.makefile()
+
+    while True:
+        hin = f.readline().strip()
+        if hin == '':
+            print "Connect closed"
+            break
+        bc = binascii.unhexlify(hin)
+        huff = assign_opcode(bc, hcode_table)
+        hout = binascii.hexlify(huff.tobytes()) + '\n'
+        print hout
+        s.sendall(hout)
+    f.close()
 
 #Compression testing with huffman codes
-hcode_table = {}
-fileName="/Users/anithagollamudi/cs222/compressEVM/test/huffmanopcodes.txt"
-with open(fileName, mode='r') as file: 
-    for line in file:
-            token = str.split(line)
-            hcode_table[inv_opcode_table[token[0]]] = token[1]
+def init_hc_table(filename):
+    hcode_table = {}
+    with open(filename, mode='r') as file:
+        for line in file:
+                token = str.split(line)
+                hcode_table[inv_opcode_table[token[0]]] = token[1]
+    return hcode_table
 
-def assign_opcode(bc):
+def assign_opcode(bc, hcode_table):
     l = list(bc)
     newl = bitarray()
     for i in l:
@@ -38,12 +58,14 @@ def assign_opcode(bc):
             newl.extend(s[2:])
     return newl
 
-
-bytecode = binascii.unhexlify(hbytecode)
-#print "Hexlified bytecode", hbytecode
-#print "Unhexlified bytecode", bytecode
-#print "end"
-newl= assign_opcode(bytecode)
-newbytecode = newl.tobytes()
-print newbytecode
-#print "hello world"
+if __name__ == '__main__':
+    hcode_table = init_hc_table(huffman_file)
+    s = start_server()
+    while True:
+        try:
+            accept_and_handle(s, hcode_table)
+        except (KeyboardInterrupt, SystemExit):
+            break
+    print 'Shutdown'
+    s.close()
+    sys.exit(0)
