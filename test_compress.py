b9ea79f82c353adc181a1dc28d3db406189a5be0
Anitha Gollamudi <agollamudi@g.harvard.edu>
2016-12-06 21:32:59 -0500

    Steps to repro the problem

--- test_compress.py
+++ test_compress.py
@@ -7,7 +7,9 @@ offset = 0
 while offset < len(data):
     group, offset = rlp.codec.consume_item(data, offset)
     for b in group[1]:
-        #if len(b[2]) >= 1:
-        #    print offset
-        #    print b[2][0]
-        print b[0][-3]
+        for t in b[2]:
+            # Transaction
+            #print offset
+            #print b[2][0]
+            print t[0]
+        #print b[0][-3]
